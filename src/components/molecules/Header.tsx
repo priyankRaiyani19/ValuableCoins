d14ce85e1691d95be02beba837e1ec14f1952595
priyankRaiyani19 <priyankraiyani19@gmail.com>
2025-04-10 12:31:02 +0530

    fix bugs in coin gecko.

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,11 +1,15 @@
+import favicon from "../../assets/Adobe Express - file.png";
 import SearchBar from "./SearchBar.tsx";
 
 function Header() {
   return (
     <div className="flex justify-between items-center w-full text-white py-6 relative px-6">
       {/* <div className="flex justify-between items-center w-full px-6"> */}
-      <div className="text-white text-3xl font-semibold tracking-wide">
-        CryptoS
+      <div className="flex  justify-center items-center gap-[0.5rem]">
+        <img src={favicon} alt="not found" />
+        <div className="text-white text-3xl font-semibold tracking-wide">
+          CryptoS
+        </div>
       </div>
       <SearchBar />
       {/* </div> */}
